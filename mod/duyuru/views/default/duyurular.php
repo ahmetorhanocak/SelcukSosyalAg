2e51daad5e9291802ad1d9665c71f6915d2002da
ahmetorhan <a.orhanocak@gmail.com>
2017-09-24 22:54:04 +0300

    Adds "Announcements" plugin

--- mod/duyuru/views/default/duyurular.php
+++ mod/duyuru/views/default/duyurular.php
@@ -1,14 +1,7 @@
 <?php
-	$url = "https://www.selcuk.edu.tr/muhendislik/bilgisayar/Web/Sayfa/Ayrinti/4642/tr";
-	$contents = file_get_contents($url);
-	$str = "Öğrenci E-Posta";
-	$str2 = "DUYURU ARŞİVİ";
 
-	$contents = strstr($contents , $str, $before_str = false);
-	$contents = strstr($contents , "Duyurular", $before_str = false);
-	$duyuru =  strstr($contents , $str2, $before_str2 = true);
-	$str3 = "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><body><div class=\"w3-container w3-teal\"></div><div class=\"w3-container\">";
-	$str4 = "</div>";
+use Sunra\PhpSimple\HtmlDomParser;
+$url = "http://www.selcuk.edu.tr/muhendislik/bilgisayar/Web/Sayfa/Ayrinti/4642/tr";
+$html = HtmlDomParser::str_get_html(file_get_contents($url));
 
-	echo str_replace('Duyurular', '<h2>Selçuk Üniversitesi Bilgisayar Mühendisliği</h2><br>', $str3.$duyuru.$str4);
-?>
+echo $html->find("span#ContentPlaceHolder1_lblIcerik", 0);
